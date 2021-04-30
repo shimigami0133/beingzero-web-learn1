1c24b7572949afeea606c5ff2725a761823a2e73
abhiram reddy <shimigami0133@gmail.com>
2021-04-30 11:02:28 +0530

    adflkkj

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express=require('express')
 const app=express()
-const pin=process.env.PORT || 3000
-app.use(express.static(__dirname+"/static"))
+const pin=process.env.PORT || 8000
 app.get('/',(req,res)=>{
     res.send("hey");
 })
+app.use(express.static(__dirname+"/static"))
 app.get('/resume',(req,res)=>
 {
     res.sendFile(__dirname+"/static/index.html")
 })
-app.listen(pin,'127.0.0.1',()=>
+app.listen(pin,()=>
 {
     console.log("server is listening"+pin)
 
