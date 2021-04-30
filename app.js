db561ed9e8df0a5995d7b2acc5c73d6d5194e9a7
abhiram reddy <shimigami0133@gmail.com>
2021-04-30 15:57:18 +0530

    aslkdjkfasl

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express=require('express')
 const app=express()
 const pin=process.env.PORT || 8000
 app.get('/',(req,res)=>{
-    res.send("hey");
+    res.sendFile(__dirname+"/static/index.html")
 })
 app.use(express.static(__dirname+"/static"))
 app.get('/resume',(req,res)=>
 {
-    res.sendFile(__dirname+"/static/index.html")
+    res.sendFile(__dirname+"/static/resume.html")
 })
 app.listen(pin,()=>
 {
--- static/index.js
+++ static/index.js
@@ -1,1 +1,4 @@
-console.log("resume")
+document.querySelector('#b1').addEventListener('click',(event)=>
+{
+    alert("hey");
+})
