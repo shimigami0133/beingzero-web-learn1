df5cfebd49b3a876819c2b77105375dd0f98f4e4
abhiram reddy <shimigami0133@gmail.com>
2021-05-06 07:34:34 +0530

    dlkljf

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const pin=process.env.PORT || 8000
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+"/static/index.html")
 })
+app.get('/color',(req,res)=>{
+    res.sendFile(__dirname+"/static/color.html")
+})
 app.get('/google',(req,res)=>{
     res.sendFile(__dirname+"/static/google.html")
+})
+app.get('/apple',(req,res)=>{
+    res.sendFile(__dirname+"/static/aapple.html")
 })
 app.get('/resume',(req,res)=>
 {
